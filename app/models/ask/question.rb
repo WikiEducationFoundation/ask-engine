74ccae04ae0a032a6e5b66e1fcec01e419311d9b
Sage Ross <ragesoss@gmail.com>
2020-04-03 13:46:04 -0700

    Add markdown rendering of Question content

--- app/models/ask/question.rb
+++ app/models/ask/question.rb
@@ -1,4 +1,15 @@
 module Ask
   class Question < ApplicationRecord
+    def self.markdown_renderer
+      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    end
+
+    def self.render_markdown(content)
+      markdown_renderer.render(content)
+    end
+
+    def html_content
+      Question.render_markdown(content)
+    end
   end
 end
--- ask.gemspec
+++ ask.gemspec
@@ -26,6 +26,5 @@ Gem::Specification.new do |spec|
   spec.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
   spec.add_dependency "rails", "~> 6.0.2", ">= 6.0.2.1"
-
-  spec.add_development_dependency "sqlite3"
+  spec.add_dependency 'redcarpet' # Markdown renderer
 end
